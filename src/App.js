d26901a7fb61eda0f78f4d351504f18a9e04b742
Tran Trong Nam <namtt@aureole.local>
2022-06-27 11:42:12 +0700

    create gitpage commit 2

--- src/App.js
+++ src/App.js
@@ -1,22 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <input type="checkbox"/>
+        <div class="menu_icon">
+            <div class="menu_line">
+
+            </div>
+        </div>
+        <div class="menu_item">
+            <li><a href="">1</a></li>
+            <li><a href="">2</a></li>
+            <li><a href="">3</a></li>
+            <li><a href="">4</a></li>
+            <li><a href="">5</a></li>
+            <li><a href="">6</a></li>
+            <li><a href="">7</a></li>
+            <li><a href="">8</a></li>
+            <li><a href="">9</a></li>
+            <li><a href="">10</a></li>
+            <li><a href="">11</a></li>
+            <li><a href="">12</a></li>
+        </div>
       </header>
     </div>
   );
